5c608f2f902a185d8fe9c55d4ee7730eb3b210d1
Tejas Navghane <tejas@e-arth.in>
2016-06-27 15:34:58 +0530

    col-lg-3 social icons

---
+++ application/views/Nav.php
@@ -0,0 +1,69 @@
+<nav class="col-md-8 col-sm-8 col-xs-8">
+    <a class="cmn-toggle-switch cmn-toggle-switch__rot  open_close" href="javascript:void(0);"><span>Menu mobile</span></a>
+    <div class="main-menu">
+        <div id="header_menu">
+            <img src="" width="240" height="40" alt="GreenLeafWildLife" data-retina="true">
+        </div>
+        <a href="#" class="open_close" id="close_in"><i class="icon_set_1_icon-77"></i></a>
+        <ul>
+            <li class="submenu">
+                <a href="<?php echo site_url('Home');?>" class="show-submenu">Home </a>
+            </li>
+            <!--  <li class="submenu">
+            <a href="javascript:void(0);" class="show-submenu">Rooms <i class="icon-down-open-mini"></i></a>
+            <ul>
+                <li><a href="rooms_list.html">Room list</a></li>
+                <li><a href="room_details.html">Room details</a></li>
+                <li><a href="room_booking.html">Room details Booking</a></li>
+            </ul>
+        </li> -->
+            <li><a href="about.html">About</a></li>
+            <li class="submenu">
+                <a href="javascript:void(0);" class="show-submenu">Activities <i class="icon-down-open-mini"></i></a>
+                <ul>
+                    <li><a href="#">All activities</a></li>
+                    <li><a href="#">Family activities</a></li>
+                    <li><a href="#">Corporate activities</a></li>
+                    <!-- <li><a href="cooking.html">Cooking local food</a></li>
+                <li><a href="farm.html">Farm activities</a></li>-->
+                </ul>
+            </li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="fullscreen_gallery.html">Gallery</a></li>
+
+            <!-- <li class="megamenu submenu">
+            <a href="javascript:void(0);" class="show-submenu-mega">Pages &amp; elements<i class="icon-down-open-mini"></i></a>
+            <div class="menu-wrapper">
+                <div class="col-md-4">
+                    <h3>Pages</h3>
+                    <ul>
+                        <li><a href="gallery_3_columns.html">Gallery 3 columns</a></li>
+                       <li><a href="gallery_4_columns.html">Gallery 4 columns</a></li>
+                        <li><a href="fullscreen_gallery.html">Gallery fullscreen</a></li>
+                        <li><a href="404.html">404 Error page</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3>Pages</h3>
+                    <ul>
+                         <li><a href="blog.html">Blog</a></li>
+                        <li><a href="blog_post.html">Blog post</a></li>
+                        <li><a href="site_launch/index.html">Site launch / Coming soon</a></li>
+                        <li><a href="shortcodes.html"><i class="icon-tools"></i> Shortcodes</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h3>Elements</h3>
+                    <ul>
+                        <li><a href="../boxed/index.html"><i class="icon-columns"></i> Boxed version</a></li>
+                        <li><a href="icon_pack_1.html"><i class="icon-inbox-alt"></i> Icon pack 1 (1900)</a></li>
+                        <li><a href="icon_pack_2.html"><i class="icon-inbox-alt"></i> Icon pack 2 (100)</a></li>
+                        <li><a href="icon_pack_3.html"><i class="icon-inbox-alt"></i> Icon pack 3(17)</a></li>
+                    </ul>
+                </div>
+            </div><!-- End menu-wrapper -->
+            <!--   </li>-->
+        </ul>
+    </div>
+    <!-- End main-menu -->
+</nav>

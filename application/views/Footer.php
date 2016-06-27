c3816eaee9be7a0ecb6b95db1626c473ef74747c
Tejas Navghane <tejas@e-arth.in>
2016-06-27 16:39:02 +0530

    contact info added

---
+++ application/views/Footer.php
@@ -0,0 +1,56 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-3">
+				<h3>Contact Us</h3>
+				<ul id="contact_details_footer">
+					<li>Greenleaf Wildlife Tours <br/> Shilpa Society, C-Wing, Flat-1002, Behind <br/> MIT College, Paud Road <br/>Pune, Maharashtra, 411038</li>
+					<li><a href="mailto:info@GreenLeafWildLife.com">greenleafwildlife@gmail.com</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-sm-3">
+				<h3>Contact Numbers</h3>
+				<ul id="contact_details_footer">
+					<li>PUNE - 9890926192 / 8806668443 <br/> 9730231119 / 9158142306 <br/> MUMBAI - 9619467826 / 9833000765 <br/> AMBOLI - 8550927088</li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-sm-3">
+				<h3>About</h3>
+				<ul>
+					<li><a href="<?php echo site_url('About');?>">About us</a></li>
+					<!-- <li><a href="#">Rooms</a></li>-->
+					<li><a href="#">Activities</a></li>
+					<li><a href="#">Contact us</a></li>
+					<li><a href="#">Gallery</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-sm-3" id="newsletter">
+				<h3>Newsletter</h3>
+				<p>Join our newsletter to keep be informed about offers and news.</p>
+				<div id="message-newsletter_2"></div>
+				<form method="post" action="assets/newsletter.php" name="newsletter_2" id="newsletter_2">
+					<div class="form-group">
+						<input name="email_newsletter_2" id="email_newsletter_2" type="email" value="" placeholder="Your mail" class="form-control">
+					</div>
+					<input type="submit" value="Subscribe" class="btn_1 white" id="submit-newsletter_2">
+				</form>
+			</div>
+		</div>
+		<!-- End row -->
+		<div class="row">
+			<div class="col-md-12">
+				<div id="social_footer">
+					<ul>
+						<li><a href="https://www.facebook.com/greenleafwildlife/" target="_blank"><i class="icon-facebook"></i></a></li>
+						<li><a href="#" target="_blank"><i class="icon-instagram"></i></a></li>
+						<li><a href="#" target="_blank"><i class="icon-pinterest"></i></a></li>
+					</ul>
+					<p>© GreenLeafWildLife 2016</p>
+				</div>
+			</div>
+		</div>
+		<!-- End row -->
+	</div>
+	<!-- End container -->
+</footer>
+<!-- End footer -->

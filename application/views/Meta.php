42298a614f277c844762a8b9ea100f9bba7645b5
Tejas Navghane <tejas@e-arth.in>
2016-06-27 14:54:44 +0530

    first push

---
+++ application/views/Meta.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="farm activities, itineraries, farm holidays, Green Leaf Wild Life, bed and breakfast, hotel, country hotel" />
+<meta name="description" content="Green Leaf Wild Life">
+<meta name="author" content="">

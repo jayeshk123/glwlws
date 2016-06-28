c02e0ce74a81fc4aee14c55d4b38636e4b45407f
jayesh <jayesh.kulkarni92@gmail.com>
2016-06-28 16:48:35 +0530

    about setup

---
+++ application/views/About.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="ie ie8"> <![endif]-->
+<!--[if IE 9]><html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html>
+<!--<![endif]-->
+
+<head>
+    <?php include("Meta.php");?>
+    <title>Green Leaf Wild Life</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- Google web fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Gochi+Hand' rel='stylesheet' type='text/css'>
+    
+    <!-- BASE CSS -->
+    <link href="css/base.css" rel="stylesheet">
+    <!-- SPECIFIC CSS -->
+    <link rel="stylesheet" href="css/weather.css" >
+
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<!--[if lte IE 8]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.</p>
+<![endif]-->
+
+	<div id="preloader">
+        <div class="sk-spinner sk-spinner-wave">
+            <div class="sk-rect1"></div>
+            <div class="sk-rect2"></div>
+            <div class="sk-rect3"></div>
+            <div class="sk-rect4"></div>
+            <div class="sk-rect5"></div>
+        </div>
+    </div>
+    <!-- End Preload -->
+
+	<div class="layer"></div>
+    <!-- Mobile menu overlay mask -->
+
+    <!-- Header================================================== -->
+    <header>
+        <div id="top_line">
+            <div class="container">
+            </div><!-- End container-->
+        </div><!-- End top line-->
+        
+        <div id="top_header">
+        	<div class="container">
+            	<div class="row">
+					<div class="col-md-4 col-sm-4 col-xs-4">
+						<div id="logo">
+							<a href="<?php echo site_url('About');?>"><img src="#"  alt="Your Logo" data-retina="true"></a>
+						</div>
+					</div>
+					<?php require("Nav.php");?>
+				</div>
+            </div>
+        </div>
+    </header><!-- End Header -->
+
+    <section class="sub_header" id="bg_general">
+        <div class="sub_header_content">
+            <div class="animated fadeInDown">
+                <h1>About</h1>
+                <p>
+                    Ridiculus sociosqu cursus neque cursus curae ante scelerisque vehicula.
+                </p>
+            </div>
+        </div>
+    </section><!-- End Section -->
+    
+    <div id="position">
+        <div class="container">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Category</a></li>
+                <li>Page active</li>
+            </ul>
+        </div>
+    </div><!-- End Position -->
+    
+    <div class="container margin_60">
+        <div class="main_title">
+            <p>
+                <img src="img/best_price.png" alt="">
+            </p>
+            <span></span>
+            <h2>About Green Leaf Wild Life</h2>
+            <p class="lead">
+                We as the team of Greenleaf are nature loving enthusiasts with similar liking and that is trailingthrough the varied beauty of nature. We belong to the different backgrounds but for more than 10 years we are following nature through camping, photography, nature presentations etc. and one fine day in 2012 we decided to develope our common interest into serious hobby. We presently organize Wild Life tours with Tiger Safari as the main attraction, Bird Watching tours, Nature as Stress buster tour for Corporates & Family categories.
+            </p>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <img src="http://greenleafwildlife.com/wp-content/uploads/2015/06/tadoba7-580x460.jpg" alt="" class="img-responsive styled">
+                        <h3>Tiger Safari</h3>
+                        <p>
+                            Esse dicit accusam duo an. Modus simul ei nec. Sea et explicari pertinacia, <strong>cu vitae bonorum</strong> vocibus nec, corpora signiferumque no vim. Ad principes scriptorem duo, te omnium gubergren eam, et eam ancillae appareat dissentiet. Quando tantas animal vis ut, mandamus voluptatum duo ne, ne odio vidit commodo has.
+                        </p>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <img src="http://greenleafwildlife.com/wp-content/uploads/2015/07/IMG_1156-580x460.jpg" alt="" class="img-responsive styled">
+                        <h3>Bird Watching</h3>
+                        <p>
+                            Esse dicit accusam duo an. Modus simul ei nec. Sea et explicari pertinacia, <strong>cu vitae bonorum</strong> vocibus nec, corpora signiferumque no vim. Ad principes scriptorem duo, te omnium gubergren eam, et eam ancillae appareat dissentiet. Quando tantas animal vis ut, mandamus voluptatum duo ne, ne odio vidit commodo has.
+                        </p>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <img src="http://greenleafwildlife.com/wp-content/uploads/2015/06/IMG-20150607-WA0016.jpg" alt="" class="img-responsive styled">
+                        <h3>Stress buster Family Tour</h3>
+                        <p>
+                            Esse dicit accusam duo an. Modus simul ei nec. Sea et explicari pertinacia, <strong>cu vitae bonorum</strong> vocibus nec, corpora signiferumque no vim. Ad principes scriptorem duo, te omnium gubergren eam, et eam ancillae appareat dissentiet.
+                        </p>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <img src="http://greenleafwildlife.com/wp-content/uploads/2015/06/IMG-20150607-WA0016.jpg" alt="" class="img-responsive styled">
+                        <h3>Stress buster Corporate Tour</h3>
+                        <p>
+                            Esse dicit accusam duo an. Modus simul ei nec. Sea et explicari pertinacia, <strong>cu vitae bonorum</strong> vocibus nec, corpora signiferumque no vim. Ad principes scriptorem duo, te omnium gubergren eam, et eam ancillae appareat dissentiet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <ul class="feat" id="about">
+                    <li>
+                    <i class="icon_set_1_icon-21"></i>
+                    <h4>A/C Cars</h4>
+                    <p>
+                         Eum purto epicurei cotidieque at, ius luptatum invidunt no, vim at sint pertinacia repudiandae. Ad cum dicant laboramus delicatissimi, ex has nonumes explicari prodesset, brute tincidunt conclusionemque no has.
+                    </p>
+                    </li>
+                    <li>
+                    <i class="icon_set_1_icon-34"></i>
+                    <h4>Photography Tips</h4>
+                    <p>
+                         Eum purto epicurei cotidieque at, ius luptatum invidunt no, vim at sint pertinacia repudiandae. Ad cum dicant laboramus delicatissimi, ex has nonumes explicari prodesset, brute tincidunt conclusionemque no has.
+                    </p>
+                    </li>
+                    <li>
+                    <i class="icon_set_1_icon-58"></i>
+                    <h4>Hygienic & healthy food options</h4>
+                    <p>
+                         Eum purto epicurei cotidieque at, ius luptatum invidunt no, vim at sint pertinacia repudiandae. Ad cum dicant laboramus delicatissimi, ex has nonumes explicari prodesset, brute tincidunt conclusionemque no has.
+                    </p>
+                    </li>
+                </ul>
+            </div>
+        </div><!-- End Row -->
+        <hr>
+        <div id="testimonials">
+            <div class='row'>
+                <div class='col-md-offset-1 col-md-10'>
+                    <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+                        <!-- Bottom Carousel Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#quote-carousel" data-slide-to="1"></li>
+                            <li data-target="#quote-carousel" data-slide-to="2"></li>
+                        </ol><!-- Carousel Slides / Quotes -->
+                        <div class="carousel-inner">
+                            <!-- Quote 1 -->
+                            <div class="item active">
+                                <blockquote>
+                                    <img class="img-circle" src="<?php echo site_url();?>/img/testimonial_1.jpg" alt="">
+                                    <p>
+                                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris."
+                                    </p>
+                                    <small>Someone famous</small>
+                                </blockquote>
+                            </div>
+                            <!-- Quote 2 -->
+                            <div class="item">
+                                <blockquote>
+                                    <img class="img-circle" src="<?php echo site_url();?>/img/testimonial_1.jpg" alt="">
+                                    <p>
+                                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris."
+                                    </p>
+                                    <small>Someone famous</small>
+                                </blockquote>
+                            </div>
+                            <!-- Quote 3 -->
+                            <div class="item">
+                                <blockquote>
+                                    <img class="img-circle" src="<?php echo site_url();?>/img/testimonial_1.jpg" alt="">
+                                    <p>
+                                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris."
+                                    </p>
+                                    <small>Someone famous</small>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- End row -->
+        </div><!-- End testimonials -->
+        
+       <div class="main_title add_bottom_45">
+            <span></span>
+            <h2>Weather forecast</h2>
+            <p>
+                Ad principes scriptorem duo, te omnium gubergren eam, et eam ancillae appareat dissentiet.
+            </p>
+        </div>
+        <div id="weather" class="clearfix"></div><!-- Weather widget -->
+ </div><!-- End Container -->
+        
+    <?php require("Footer.php");?>
+
+<div id="toTop"></div><!-- Back to top button -->
+
+<!-- Common scripts -->
+<script src="<?php echo site_url();?>/js/jquery-1.11.2.min.js"></script>
+<script src="<?php echo site_url();?>/js/common_scripts_min.js"></script>
+<script src="<?php echo site_url();?>/js/functions.js"></script>
+<script src="<?php echo site_url();?>/assets/validate.js"></script>
+ 
+ <!-- Specific scripts -->   
+<script src="<?php echo site_url();?>/js/jquery.simpleWeather.min.js"></script> 
+<script>
+// v3.1.0
+//Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Marseille, France',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<h4><i class="weathericon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h4>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+	  
+	   for(var i=0;i<weather.forecast.length;i++) {
+        html += '<span class="details_forecast">'+weather.forecast[i].day+': '+weather.forecast[i].high+'</span>';
+      }
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
+</script>
+
+</body>
+
+</html>
